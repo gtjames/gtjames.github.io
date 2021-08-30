1f516f4ba2e8ccf0d25cfb81369b23a2cdfe1c0e
gtjames <gtjames@gmail.com>
2021-08-30 14:10:57 -0500

    modernized the JS. modernized the styling of the tables. Now using the style attribute

--- js/dom.js
+++ js/dom.js
@@ -7,33 +7,18 @@
 // Warning: This script is provided "as is" and without warranty of any kind.
 // Use at your own risk.
 
-
-function runTest()
-{
-	var demoOutputWindow=window.open();
-	var myOutputWindow=window.open();
-	var myDocument=myOutputWindow.document.open("text/html", "replace");
-	myDocument.writeln("<HTML ID='testhtml'><head><title>Html Title</TITLE></HEAD>");
-	myDocument.writeln("<BODY><TABLE><TR><TD>Table cell 1</TD></TR><TR><TD>Table cell 2</TD></TR></TABLE></BODY>");
-	myDocument.writeln("</HTML>");
-	myDocument.close();
-	printDOMTree(myDocument.getElementById("testhtml"), demoOutputWindow);
-	myOutputWindow.close();
-	demoOutputWindow.focus();
-}
-
 ////////////////////////////////////////////
 // This function accepts a DOM element as parameter and prints
 // out the descendant DOM structure of the element on a new window.
 ////////////////////////////////////////////
 function printDOMTree(domElement, destinationWindow)
 {
-	var outputWindow=destinationWindow;
+	let outputWindow=destinationWindow;
 	if (!outputWindow)
 		outputWindow=window.open();
 	outputWindow.document.open("text/html", "replace");
 
-	var myElement=domElement;
+	let myElement=domElement;
 	outputWindow.document.write("<HTML><HEAD><TITLE>Dom Tree</TITLE></HEAD><BODY>\n");
 	outputWindow.document.write("<CODE>\n");
 	traverseDOMTree(outputWindow.document, myElement, 1);
@@ -57,14 +42,14 @@ function traverseDOMTree(targetDocument, currentElement, depth)
 {
 	if (currentElement)
 	{
-		var j;
-		var tagName=currentElement.tagName;
-		targetDocument.write( (tagName) ?	"<SPAN STYLE='color:darkred;'>&lt;"+currentElement.tagName+"&gt;&nbsp;</SPAN>" : 
+		let j;
+		let tagName=currentElement.tagName;
+		targetDocument.write( (tagName) ? "<SPAN STYLE='color:darkred;'>&lt;"+currentElement.tagName+"&gt;&nbsp;</SPAN>" :
 									"<SPAN STYLE='color:darkgreen;'>"+(currentElement.nodeName + ":" + currentElement.nodeValue.replace("<","&lt;") )+"</SPAN>");
 		if (currentElement.attributes && currentElement.attributes.length > 0)
 			showAttributes(targetDocument, currentElement);
 
-		for ( var i = 0, currentElementChild = currentElement.childNodes[i]; currentElementChild; i++, currentElementChild=currentElement.childNodes[i] )
+		for ( let i = 0, currentElementChild = currentElement.childNodes[i]; currentElementChild; i++, currentElementChild=currentElement.childNodes[i] )
 		{
 			targetDocument.write("<BR>");
 			for (j=0; j<depth; j++)
@@ -95,7 +80,7 @@ function traverseDOMTree(targetDocument, currentElement, depth)
 function showAttributes(targetDocument, currentElement)
 {
 	targetDocument.writeln("<SPAN STYLE='color:blue;'>[");
-	for (var i = 0, atts = currentElement.attributes, n = atts.length; i < n; i++)
+	for (let i = 0, atts = currentElement.attributes, n = atts.length; i < n; i++)
 	{
 		targetDocument.writeln("("+atts[i].nodeName + ":" + atts[i].nodeValue + ")	 ");
 	}
